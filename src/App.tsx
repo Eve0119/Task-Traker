bc949373c74bfdc1c8506e0be4f783430695cdc4
Eve0119 <gerictorilla@gmail.com>
2025-06-05 10:30:08 +0800

    create and read on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import NewTask from "./components/NewTask";
 import AllTasks from "./components/AllTasks";
+import type { Task } from "./types/type";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (taskName: string) => {
+    const newTask: Task = {
+      id: Date.now(), // Unique ID for the task
+      taskName,
+      isComplete: false,
+      dateCreated: new Date(),
+      dateCompleted: null,
+    };
+    setTasks((prev) => [...prev, newTask]);
+  };
+
   return (
     <>
       <Header />
       {/* Your other components */}
       <main>
-        <NewTask />
-        <AllTasks />
+        <NewTask onAddTask={addTask} />
+        <AllTasks tasks={tasks} />
         {/* Add more components here as needed */}
       </main>
     </>
